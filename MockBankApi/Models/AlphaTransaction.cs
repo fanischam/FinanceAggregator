2d054832fb526955a22a50c888fadb19960da22d
Fanis Chamalidis <fchamalidis@yahoo.com>
2026-03-27 22:01:35 +0200

    Created Alpha transaction record

---
+++ MockBankApi/Models/AlphaTransaction.cs
@@ -0,0 +1,10 @@
+﻿namespace MockBankApi.Models
+{
+    record AlphaTransaction(
+        string TransactionId,
+        string AccountId,
+        decimal Amount,
+        string Currency,
+        string MerchantName,
+        string Date);
+}
