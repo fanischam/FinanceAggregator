0abf40cc36218d07947ff11881b5270a42407647
Fanis Chamalidis <fchamalidis@yahoo.com>
2026-03-27 22:05:05 +0200

    Created Beta and Gamma models

---
+++ MockBankApi/Models/BetaTransaction.cs
@@ -0,0 +1,10 @@
+﻿namespace MockBankApi.Models
+{
+    public record BetaTransaction(
+        string transaction_id,
+        string account_id,
+        int amount_cents,
+        string currency_code,
+        string vendor_name,
+        string transaction_date);
+}
---
+++ MockBankApi/Models/GammaTransaction.cs
@@ -0,0 +1,10 @@
+﻿namespace MockBankApi.Models
+{
+    record GammaTransaction(
+        int id,
+        string AccountNumber,
+        string Value,
+        string CurrencyCode,
+        string Description,
+        String PostedAt);
+}
