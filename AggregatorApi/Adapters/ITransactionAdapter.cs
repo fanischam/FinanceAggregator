58772273d5da4eed903f73d76f8a9edd7d6d73af
Fanis Chamalidis <fchamalidis@yahoo.com>
2026-03-28 22:12:45 +0200

    Implemented Transaction Adapter interface to map different transaction types

---
+++ AggregatorApi/Adapters/ITransactionAdapter.cs
@@ -0,0 +1,10 @@
+﻿using AggregatorApi.Models;
+
+namespace AggregatorApi.Adapters
+{
+    public interface ITransactionAdapter
+    {
+        string BankName { get; }
+        Task<IEnumerable<Transaction>> GetTransactionsAsync(string accountId, CancellationToken ct = default);
+    }
+}
