bd0a50181f665c9879bca198d952052776f3e0d1
Fanis Chamalidis <fchamalidis@yahoo.com>
2026-03-28 21:54:04 +0200

    Created Transaction model on Aggregator API

---
+++ AggregatorApi/Models/Transaction.cs
@@ -0,0 +1,13 @@
+﻿namespace AggregatorApi.Models
+{
+    public class Transaction
+    {
+        public string Id { get; init; } = default!;
+        public string AccountId { get; init; } = default!;
+        public string BankSource { get; init; } = default!;
+        public decimal AmountEur { get; init; }
+        public string Currency { get; init; } = default!;
+        public string MerchantName { get; init; } = default!;
+        public DateTimeOffset PostedAt { get; init; }
+    }
+}
